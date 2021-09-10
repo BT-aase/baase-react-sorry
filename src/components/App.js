7c491468aa1221122e8cdfccecc7ad6f04137954
Brandon Aase <baase@catalyte.io>
2021-09-10 16:02:28 -0700

    Added Electron and started experimenting with elements.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div style={{
+			display: "flex",
+			alignItems: "center",
+			justifyContent: 'center',
+			height: "100vh"
+		}}>
+			<div style={{height: 600, width: 600, backgroundColor: '#cce3be'}}></div>
+		</div>
+
+	);
+};
+
+export default App;
