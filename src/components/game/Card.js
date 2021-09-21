07237d824307415f6d40e312ec4a2b21aaa2f54f
Brandon Aase <baase@catalyte.io>
2021-09-21 14:31:48 -0700

    Added styling for front of card & start of function for card display.

--- src/components/game/Card.js
+++ src/components/game/Card.js
@@ -4,13 +4,30 @@ const Card = (props) => {
 
     const cardDisplay = (number) => {
         let display;
+        let text;
+
+        switch(number) {
+            case 1:
+                text = 'Must either start a man out or move one man forward 1 square.'
+                break;
+            default:
+                break;
+        }
+
 
         typeof (number) !== 'undefined' ?
             display = (
                 <div style={{ width: 220, height: 140, backgroundColor: 'white', borderRadius: 10 }}>
-                    <div style={{ position: 'fixed', transform: 'rotate(270deg)', left: 210, top: 190 }}>
-                        {/* <p style={{ fontFamily:'Purple Purse', fontSize: 22, paddingTop: -30 }}>1</p> */}
-                        <p style={{ width: 95, fontSize: 10 }}>Must either start a man out or move one man forward 1 square.</p>
+                    <div style={{ display: 'flex', position: 'fixed', transform: 'rotate(270deg)', left: 190, top: 210 }}>
+                        <p style={{ fontFamily: 'Purple Purse', fontSize: 22, paddingRight: 5 }}>{number}</p>
+                        <p style={{ width: 115, fontSize: 9, paddingTop: 5 }}>{text}</p>
+                    </div>
+                    <div style={{position: 'fixed', transform: 'rotate(270deg)', left: 325, top: 190}}>
+                        <p style={{ fontFamily: 'Purple Purse', fontSize: 60 }}>1</p>
+                    </div>
+                    <div style={{ display: 'flex', position: 'fixed', transform: 'rotate(90deg)', left: 340, top: 210 }}>
+                        <p style={{ fontFamily: 'Purple Purse', fontSize: 22, paddingRight: 5 }}>{number}</p>
+                        <p style={{ width: 115, fontSize: 9, paddingTop: 5 }}>{text}</p>
                     </div>
                 </div>
             ) :
