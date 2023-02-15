ad5649054cb89ff6e724d3cef7c4007f3f7b298f
Brandon Aase <baase@catalystsolves.com>
2023-02-15 15:13:13 -0800

    Set up app on new computer & add shadow for 3D effect.

--- src/components/game/elements/GamePiece.js
+++ src/components/game/elements/GamePiece.js
@@ -2,6 +2,13 @@ import React from "react";
 
 import colors from "../../colors";
 
+const shadeColors = {
+    red: '#cc0000',
+    blue: '#1050c3',
+    yellow: '#cccc00',
+    green: '#006600',
+}
+
 const GamePiece = (props) => {
 
     let color = colors[props.color];
@@ -15,7 +22,8 @@ const GamePiece = (props) => {
             <div style={{
                 width: 15, height: 15, border: '1px solid black',
                 borderRadius: 20, backgroundColor: color,
-                marginTop: 6.5, marginLeft: 6.5
+                marginTop: 6.5, marginLeft: 6.5,
+                boxShadow: `0 0 0 2px ${shadeColors[props.color]}`
             }}></div>
         </div>
     )
